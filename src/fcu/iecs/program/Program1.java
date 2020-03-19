c609cfad9f4145d81429eaab877360f9d36b86a5
user <user@FF03>
2020-03-19 14:06:38 +0800

    start

---
+++ src/fcu/iecs/program/Program1.java
@@ -0,0 +1,15 @@
+package fcu.iecs.program;
+
+import java.util.Scanner;
+
+public class Program1 {
+
+  public static void main(String[] args) {
+    System.out.print("Push");
+    Scanner Scanf = new Scanner(System.in);
+    int i;
+    i = Scanf.nextInt();
+    System.out.printf("%d",i);
+  }
+
+}
